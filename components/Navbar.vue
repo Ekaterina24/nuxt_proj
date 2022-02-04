88073fc6e759293c61ed865c3110a67ff2126cd3
Ekaterina Rykova <katrykova2002@yandex.ru>
2022-02-04 09:53:03 +0300

    proj rewrite on nuxt

---
+++ components/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <v-toolbar
+      color="purple lighten-1"
+      flat
+    >
+      <v-img
+        alt="logo"
+        src="LEGO-Logo.png"
+        max-width="70"
+      ></v-img>
+      <v-toolbar-items>
+        <v-btn
+          text
+          color="#FFFFFF"
+        >
+          <nuxt-link
+            to="/"
+            color="#FFFFFF"
+          >
+            Главная
+          </nuxt-link>
+        </v-btn>
+        <v-btn
+          text
+          color="#FFFFFF"
+        >
+          <nuxt-link to="/posts">Новости</nuxt-link>
+        </v-btn>
+        <v-btn
+          text
+          color="#FFFFFF"
+        >
+          <nuxt-link to="/about">О нас</nuxt-link>
+        </v-btn>
+        <v-btn
+          text
+          color="#FFFFFF"
+        >
+          <nuxt-link to="/gallery">Галерея</nuxt-link>
+        </v-btn>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+.v-application a {
+  color: white!important;
+  text-decoration: none;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <div class="wrapper">
+      <h1>Мы рады вас приветствовать на нашем сайте!</h1>
+      <Slider />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Slider from "../components/Slider";
+
+export default {
+  name: 'index',
+  components: {Slider}
+}
+</script>
+
+<style scoped>
+.wrapper {
+  padding: 20px;
+}
+h1 {
+  margin: 0;
+  font-size: 30px;
+  font-family: "Arial", sans-serif;
+}
+</style>
